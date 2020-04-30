16769a66c21dc63721a10094ee596536d25486c5
jiangbin <1154019877@qq.com>
2020-04-30 21:00:44 +0800

    首次提交

---
+++ aalibrary/src/main/java/com/kotlin/aalibrary/MyName.java
@@ -0,0 +1,62 @@
+package com.kotlin.aalibrary;
+
+/**
+ * @author: jiangbin
+ * @date: 2020/4/30
+ */
+public class MyName {
+    public static String A = "A";
+    public static String B = "A";
+    public static String C = "A";
+    public static String D = "A";
+    public static String E = "A";
+    public static String F = "A";
+
+    public static String getA() {
+        return A;
+    }
+
+    public static void setA(String a) {
+        A = a;
+    }
+
+    public static String getB() {
+        return B;
+    }
+
+    public static void setB(String b) {
+        B = b;
+    }
+
+    public static String getC() {
+        return C;
+    }
+
+    public static void setC(String c) {
+        C = c;
+    }
+
+    public static String getD() {
+        return D;
+    }
+
+    public static void setD(String d) {
+        D = d;
+    }
+
+    public static String getE() {
+        return E;
+    }
+
+    public static void setE(String e) {
+        E = e;
+    }
+
+    public static String getF() {
+        return F;
+    }
+
+    public static void setF(String f) {
+        F = f;
+    }
+}
---
+++ app/src/main/java/com/kotlin/newtest/MainActivity.java
@@ -0,0 +1,16 @@
+package com.kotlin.newtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kotlin.aalibrary.MyName;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
